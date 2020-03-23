3a4c0489bdb8400109f06ca22bf1790cca935854
Hilde <hilde.gynoid@gmail.com>
2020-03-23 21:50:24 +0900

    Update JDK

--- scripts/install_jdk_mac.sh
+++ scripts/install_jdk_mac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function jdk14() {
-  URL="https://download.java.net/java/early_access/jdk14/33/GPL/openjdk-14-ea+33_osx-x64_bin.tar.gz"
+  URL="https://download.java.net/java/GA/jdk14/076bab302c7b4508975440c56f6cc26a/36/GPL/openjdk-14_osx-x64_bin.tar.gz"
   curl -L -o openjdk14.tgz $URL
   if [ $? -ne 0 ]; then
     echo JDK downloading failed from $URL
@@ -17,22 +17,4 @@ function jdk14() {
   mv jdk-14.jdk $HOME/jdk-14
 }
 
-function jdk13() {
-  URL="https://download.java.net/java/GA/jdk13.0.2/d4173c853231432d94f001e99d882ca7/8/GPL/openjdk-13.0.2_osx-x64_bin.tar.gz"
-  curl -L -o openjdk13.tgz $URL
-  if [ $? -ne 0 ]; then
-    echo JDK downloading failed from $URL
-    exit 1
-  fi
-
-  tar zxvf openjdk13.tgz
-  if [ $? -ne 0 ]; then
-    echo JDK extracting failed.
-    exit 1
-  fi
-  rm openjdk13.tgz
-  mv jdk-13.0.2.jdk $HOME/jdk-13
-}
-
-jdk13
 jdk14
--- scripts/install_jdk_windows.sh
+++ scripts/install_jdk_windows.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function jdk14() {
-  URL="https://download.java.net/java/early_access/jdk14/33/GPL/openjdk-14-ea+33_windows-x64_bin.zip"
+  URL="https://download.java.net/java/GA/jdk14/076bab302c7b4508975440c56f6cc26a/36/GPL/openjdk-14_windows-x64_bin.zip"
   curl -L -o openjdk14.zip $URL
   if [ $? -ne 0 ]; then
     echo JDK downloading failed from $URL
@@ -17,22 +17,4 @@ function jdk14() {
   mv jdk-14 $HOME/jdk-14
 }
 
-function jdk13() {
-  URL="https://download.java.net/java/GA/jdk13.0.2/d4173c853231432d94f001e99d882ca7/8/GPL/openjdk-13.0.2_windows-x64_bin.zip"
-  curl -L -o openjdk13.zip $URL
-  if [ $? -ne 0 ]; then
-    echo JDK downloading failed from $URL
-    exit 1
-  fi
-
-  unzip openjdk13.zip
-  if [ $? -ne 0 ]; then
-    echo JDK extracting failed.
-    exit 1
-  fi
-  rm openjdk13.zip
-  mv jdk-13.0.2 $HOME/jdk-13
-}
-
-jdk13
 jdk14
