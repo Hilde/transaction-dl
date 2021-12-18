e2649ca56562fd4c7e64352116946e6fe623aa76
Hilde <hilde.gynoid@gmail.com>
2021-12-18 15:26:37 +0900

    Fix

--- scripts/install_jdk_windows.sh
+++ scripts/install_jdk_windows.sh
@@ -4,6 +4,7 @@ BASEDIR=$(dirname $0)
 WORKDIR=$BASEDIR/../work
 
 function jdk() {
+  mkdir -p $WORKDIR
   pushd $WORKDIR
   VERSION="17.0.1+12"
   URL="https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.1%2B12/OpenJDK17U-jdk_x64_windows_hotspot_17.0.1_12.zip"
